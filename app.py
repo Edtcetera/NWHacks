742401f0b704c2536924337afc61a24ea60487ed
Anna Leong <awyleong@interchange.ubc.ca>
2016-05-03 13:09:49 -0700

    Cleaned up front-end to remove alert boxes and choices that weren't implemented.

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ def getact():
     lati = request.form['lat']
     longi = request.form['lon']
     distance = int(request.form['distance'])
+    if (distance is None):
+        distance = 1
     distance = distance*1000
     dist = str(distance)
     print(acttype)
@@ -23,7 +25,7 @@ def getact():
         url = "https://developers.zomato.com/api/v2.1/search?lat="+lati+"&lon="+longi+"&radius="+dist+"&apikey=40b2fe93e6a1e1e123e6f67b846a5696"
         response = urllib.urlopen(url)
         data = json.loads(response.read())
-        print data
+        #print data
         amount = int(data["results_shown"]);
         listOfLatLon = [];
         for i in range (0,amount):
@@ -36,7 +38,7 @@ def getact():
         user = {'nickname': "Anna"}
         return render_template('index2.html',title=name,user=user,latitude=lati,longitude=longi,listOf=listOfLatLon)
 
-    elif acttype=="bus":
+    elif acttype=="washroom":
         url = "http://amenimaps.com/amenimapi.php?amenity=toilet&mylat="+lati+"&mylon="+longi+"&mode=json&name=rara_pirates&key=773afa6b5638d5ce24e12e9acbe30bb2"
         response = urllib.urlopen(url)
         data = json.loads(response.read())
